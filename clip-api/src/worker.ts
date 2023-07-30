6ff6275b1166639e4d8abc322287289a68267b16
hytkgami <gagagatrump@gmail.com>
2023-07-30 21:45:38 +0900

    use hono

--- clip-api/src/worker.ts
+++ clip-api/src/worker.ts
@@ -1,20 +1,19 @@
-export interface Env {
-	DB: D1Database;
+import { Hono } from 'hono'
+
+type Bindings = {
+	DB: D1Database
 }
 
-export default {
-	async fetch(request: Request, env: Env) {
-		const { pathname } = new URL(request.url);
-		if (pathname === "/api/beverages") {
-			const { results } = await env.DB.prepare(
-				"SELECT * FROM Customers WHERE CompanyName = ?"
-			)
-				.bind("Bs Beverages")
-				.all();
-			return Response.json(results);
-		}
-		return new Response(
-			"Call /api/beverages to see everyone who works at Bs Beverages"
-		);
-	},
-};
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get("/api/beverages", async (c) => {
+	try {
+		const { results } = await c.env.DB.prepare("SELECT * FROM Customers WHERE CompanyName = ?").bind("Bs Beverages").all()
+		return c.json(results)
+	} catch (e) {
+		return c.json({ err: e }, 500)
+	}
+})
+app.get("*", (c) => c.json("Call /api/beverages to see everyone who works at Bs Beverages"))
+
+export default app
