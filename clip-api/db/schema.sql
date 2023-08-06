2d8ce91bdac2a0a21a9c60d41f9f5bbe05a9ba1f
hytkgami <gagagatrump@gmail.com>
2023-08-06 17:56:16 +0900

    add unique constraint to url field, insert or replace when put an article

--- clip-api/db/schema.sql
+++ clip-api/db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS articles (
 	id TEXT NOT NULL PRIMARY KEY,
 	title TEXT NOT NULL,
-	url TEXT NOT NULL,
+	url TEXT NOT NULL UNIQUE,
 	created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
 	updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
--- clip-api/src/worker.ts
+++ clip-api/src/worker.ts
@@ -1,39 +1,39 @@
-import { Hono } from 'hono'
-import { validator } from 'hono/validator'
-import { schema } from './domain/article'
-import { generateId } from './util/id'
+import { Hono } from 'hono';
+import { validator } from 'hono/validator';
+import { schema } from './domain/article';
+import { generateId } from './util/id';
 
 type Bindings = {
-  DB: D1Database
-}
+  DB: D1Database;
+};
 
-const app = new Hono()
+const app = new Hono();
 app.get('/ping', (c) => {
-  return c.text('pong')
-})
+  return c.text('pong');
+});
 
-const api = new Hono<{ Bindings: Bindings }>()
+const api = new Hono<{ Bindings: Bindings }>();
 
 api.put(
   '/articles',
   validator('json', (value, c) => {
-    const parsed = schema.safeParse(value)
+    const parsed = schema.safeParse(value);
     if (!parsed.success) {
-      return c.json({ err: parsed.error }, 400)
+      return c.json({ err: parsed.error }, 400);
     }
-    return parsed.data
+    return parsed.data;
   }),
   async (c) => {
-    const body = c.req.valid('json')
-    const id = generateId()
+    const body = c.req.valid('json');
+    const id = generateId();
     try {
-      await c.env.DB.prepare('INSERT INTO articles (id, title, url) VALUES (?, ?, ?)').bind(id, body.title, body.url).run()
-    } catch (error) {
-      return c.json({ error }, 500)
+      await c.env.DB.prepare('INSERT OR REPLACE INTO articles (id, title, url) VALUES (?, ?, ?)').bind(id, body.title, body.url).run();
+    } catch (error: any) {
+      return c.json({ error }, 500);
     }
-    return c.json({ id }, 200)
+    return c.json({ id }, 200);
   }
-)
+);
 
-app.route('/api', api)
-export default app
+app.route('/api', api);
+export default app;
